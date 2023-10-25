f30196b945440efe4bc416655d112355ee9e3535
Masuma01~ <javariamasooma.bscssef20@iba-suk.edu.pk>
2023-10-25 13:40:39 +0500

    added some styles

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+function submitted(){
+    const name = document.getElementById('name');
+    const phone = document.getElementById('phone');
+    const email = document.getElementById('email');
+    const address = document.getElementById('address');
+    const description = document.getElementById('description');
+
+    console.log(name.value, email.value, phone.value, address.value, description.value);
+}
